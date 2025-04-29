bb69cb9ec89a34405d82268975a0f3a808c245fb
Diego Cruces <diegocruces1002@gmail.com>
2025-04-29 10:14:29 -0400

    modelo libro terminado

--- bibliotecaduoc/src/main/java/com/example/bibliotecaduoc/model/Libro.java
+++ bibliotecaduoc/src/main/java/com/example/bibliotecaduoc/model/Libro.java
@@ -1,8 +1,20 @@
 package com.example.bibliotecaduoc.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 public class Libro {
     @Data
-    @all
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class Libro {
+
+        private int id;
+        private String isbn;
+        private String titulo;
+        private String editorial;
+        private int fechaPublicacion;
+        private String autor;
+    }
 }
